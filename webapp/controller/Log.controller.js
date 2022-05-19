cf9f6b8f0f8ed05b4d53b83e3a8daa4df1bfd142
Attila Berencsi <attilaberencsi@gmail.com>
2022-05-19 23:06:14 +0200

    Cosmetics

--- webapp/controller/Log.controller.js
+++ webapp/controller/Log.controller.js
@@ -4,10 +4,19 @@ sap.ui.define([
     ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
-     * Samples to log messages in teh console and set the maximum log level (type of console messages Error/Warning etc.) logged in teh console.
-     * (HINT: do not forget to select the required messages types to be shown in teh browser console).
-     * Beside below examples: You can also use  sap/base/Log.setLevel to set log level programmatically for dedicated components only or globally.     
-     * Read detailed documentation of log API at https://sapui5.hana.ondemand.com/#/api/module:sap/base/Log
+     * Samples to log messages in the console, and to set the maximum log level (type of console messages Error/Warning(2)/Info(3)/Debug(4)...the level 
+     * until including all previous levels they are written to the console.
+     * 
+     * The Log Level can be set by following ways:
+     * - using the URL parameter sap-ui-logLevel=<logLevel>
+     * - if You have an index.html, You can set the following property in the UI5 bootstrap script as well:  data-sap-ui-loglevel="debug" forex
+     *   (all the existing log levels are enumareated in: sap/base/Log.Level)
+     * - programatically: Log.setLevel(4). You can use this statement in the console during debugging as well, to switch the log level
+     * 
+     * HINT: do not forget to select the required messages types to be shown in the browser console settings of the developer tools.
+     * 
+     * Beside below examples: You can also use  sap/base/Log.setLevel to set log level programmatically for dedicated components only (not globally).
+     * Detailed documentation of log API at https://sapui5.hana.ondemand.com/#/api/module:sap/base/Log
      */
     function (Controller, Log) {
         "use strict";
@@ -22,17 +31,7 @@ sap.ui.define([
             },
 
             /**
-             * Info messages are not whown by default. Add URL parameter  sap-ui-logLevel=3  to see them in the console.
-             * You can add some prefix like SAPDEV: to find Your log entries easier.
-             */
-            onLogInfo: function () {
-                debugger;
-                // You can use this statement in the console as well to set the Log level (:   Log.setLevel(3);
-                Log.info(`SAPDEV: Hi this is an INFO message, to see it ${this._sShowInfoinConsole}`, "Here comes the long description, like ${this._sInfo}");
-            },
-
-            /**
-             * Error Messages are logged to the console always, callstack is included also.
+             * Error Messages are logged to the console always, callstack is included by default.
              */
             onLogError: function () {
                 try {
@@ -44,11 +43,33 @@ sap.ui.define([
             },
 
             /**
-             * Debug messages are logged to the console when url parameter  sap-ui-debug=true  is set, or log level is increased using the URL parameter.
-             * If You have an index.html, You can set the following property in the UI5 bootstrap script as well:  data-sap-ui-loglevel="debug".             
+             * Wanrning messages are NOT written to the console by default, and contain no callstack. 
+             * Add URL parameter  sap-ui-logLevel=2  to see them in the console foer example.
+             * You can add some prefix like SAPDEV: to find Your log entries easier.
+             */
+            onLogWarning: function () {
+                Log.warning("SAPDEV: Hi this is a Warning message", `Here comes the long description, like ${this._sInfo}`);
+            },
+
+            /**
+             * Info messages are not whown by default, it requires to increase log level to 3 forex adding URL parameter  sap-ui-logLevel=3  to see them in the console.
+             * You can add some prefix like SAPDEV: to find Your log entries easier.
+             */
+            onLogInfo: function () {
+                // You can use this statement programatically, or in the console as well to set the required Log level (:
+                Log.setLevel(3);
+                Log.info(`SAPDEV: Hi this is an INFO message, to see it ${this._sShowInfoinConsole}`, `Here comes the long description, like ${this._sInfo}`);
+            },
+
+            /**
+             * Debug messages include call stack and are logged to the console when url parameter  sap-ui-debug=true  is set, 
+             * or the log level is increased to 4. 
+             * The UI5 logger will then  dump error/warning/info messages as well.
              */
             onLogDebug: function () {
+                //Log.setLevel(4);
                 Log.debug(`SAPDEV: Hi this is an DEBUG message`, "Bla bla");
             }
+
         });
     });
